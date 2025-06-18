1735b23339d4a62c084a206c42ff4cb68ecd3bbf
keaikelly <keaikelly@naver.com>
2025-06-19 05:17:30 +0900

    [feat] 프록시 재세팅

--- frontend/src/api/instance.js
+++ frontend/src/api/instance.js
@@ -3,15 +3,15 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_URL;
 
 const defaultInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "/api",
 });
 
 // user 인스턴스
 const userInstance = axios.create(defaultInstance.defaults);
-userInstance.defaults.baseURL += "/api/user";
+userInstance.defaults.baseURL += "/user";
 
 // tale 인스턴스
 const taleInstance = axios.create(defaultInstance.defaults);
-taleInstance.defaults.baseURL += "/api/tales";
+taleInstance.defaults.baseURL += "/tales";
 
 export { taleInstance, userInstance };
